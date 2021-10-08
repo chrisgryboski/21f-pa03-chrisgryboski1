930f8f4f4211c1029d6e03abe1746213fa5ab99f
chrisgryboski <cgryboski@smu.edu>
2021-10-07 22:25:39 -0500

    Write up draft

--- main.cpp
+++ main.cpp
@@ -6,97 +6,51 @@
 int main() {
 
 //make some data to sort
-for(int i=0; i<20; i++){
-int sorted[75000];
-int count=0;
-for (int z = 75000; z >0; z--){
-sorted[count] = z;
-count++;
-}
+//    int sorted[75000];
+//    for(int i=0; i<75000; i++){
+//        sorted[i]= rand()*75000;
+//    }
+//    int saved[75000];
+//    for(int i=0; i<75000; i++){
+//        saved[i]= sorted[i];
+//    }
+for(int i=0; i<20; i++) {
+    int sorted[75000];
+    for(int j=0; j<75000; j++){
+        sorted[j]= rand()*75000;
+    }
+//    int count = 0;
+//    for (int z = 75000; z > 0; z--) {
+//        sorted[count] = z;
+//        count++;
+//    }
 //}
 //    for(int i=0; i<75000; i++){
 //        std::cout<<sorted[i]<<std::endl;
 //    }
 
 
-int trial =20;
+    int trial = 20;
 
-std::cout << std::fixed << std::showpoint;
-std::cout.precision(9);
-print_version_number();
+    std::cout << std::fixed << std::showpoint;
+    std::cout.precision(9);
+  //  print_version_number();
 //create a start and end time_point object
-std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
 
-std::cout<<"MYSTERY SORT 1"<<std::endl;
+   // std::cout << "MYSTERY SORT 1" << std::endl;
 //print the trial number
 //  std::cout << "Trial: " << j + 1 << std::endl;
 
 
 //get the current time, run the algo, then get the current time again
-start = std::chrono::high_resolution_clock::now();
-mystery01(sorted, 75000);
-end = std::chrono::high_resolution_clock::now();
-
+    start = std::chrono::high_resolution_clock::now();
+    mystery05(sorted, 75000);
+    end = std::chrono::high_resolution_clock::now();
 //calculate the duration between start and end and print to the terminal
-std::chrono::duration<double> time_in_seconds = end - start;
-std::cout << std::fixed  << time_in_seconds.count() << std::endl;
-std::cout <<std::endl<<std::endl;
-std::cout<<"MYSTERY SORT 2"<<std::endl;
-//print the trial number
-//   std::cout << "Trial: " << j + 1 << std::endl;
-
-
-//get the current time, run the algo, then get the current time again
-start = std::chrono::high_resolution_clock::now();
-mystery02(sorted, 75000);
-end = std::chrono::high_resolution_clock::now();
-
-//calculate the duration between start and end and print to the terminal
-time_in_seconds = end - start;
-std::cout << std::fixed << time_in_seconds.count() << std::endl;
-std::cout <<std::endl<<std::endl;
-std::cout<<"MYSTERY SORT 3"<<std::endl;
-//print the trial number
-//   std::cout << "Trial: " << j + 1 << std::endl;
-
-
-//get the current time, run the algo, then get the current time again
-start = std::chrono::high_resolution_clock::now();
-mystery03(sorted, 75000);
-end = std::chrono::high_resolution_clock::now();
-
-//calculate the duration between start and end and print to the terminal
-time_in_seconds = end - start;
-std::cout << std::fixed  << time_in_seconds.count() << std::endl;
-
-std::cout <<std::endl<<std::endl;
-std::cout<<"MYSTERY SORT 4"<<std::endl;
-//print the trial number
-//   std::cout << "Trial: " << j + 1 << std::endl;
-
-
-//get the current time, run the algo, then get the current time again
-start = std::chrono::high_resolution_clock::now();
-mystery04(sorted, 75000);
-end = std::chrono::high_resolution_clock::now();
-
-time_in_seconds = end - start;
-std::cout << std::fixed << time_in_seconds.count() << std::endl;
-
-std::cout <<std::endl<<std::endl;
-std::cout<<"MYSTERY SORT 5"<<std::endl;
-//print the trial number
-//   std::cout << "Trial: " << j + 1 << std::endl;
-
-
-//get the current time, run the algo, then get the current time again
-start = std::chrono::high_resolution_clock::now();
-mystery05(sorted, 75000);
-end = std::chrono::high_resolution_clock::now();
-
-//calculate the duration between start and end and print to the terminal
-time_in_seconds = end - start;
-std::cout << std::fixed  << time_in_seconds.count() << std::endl;
+    std::chrono::duration<double> time_in_seconds = end - start;
+    std::cout << std::fixed << time_in_seconds.count() << std::endl;
+  //  std::cout << std::endl << std::endl;
 }
 //        start = std::chrono::high_resolution_clock::now();
 //        mystery02(sorted, 50000);
